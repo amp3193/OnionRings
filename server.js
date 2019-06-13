5d5d4f7738c4cea6b2c946b2bba0d16e651efa0f
amp3193 <amp3193@yahoo.com>
2019-06-12 23:28:21 -0400

    added uri info

--- server.js
+++ server.js
@@ -15,7 +15,9 @@ app.use(express.static("public"));
 require("./routes/api-routes.js")(app);
 require("./routes/html-routes.js")(app);
 
-mongoose.connect("mongodb://localhost/newsdb", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/newsdb"
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 app.listen(PORT, function() {
     console.log("App running on port " + PORT + "!");
